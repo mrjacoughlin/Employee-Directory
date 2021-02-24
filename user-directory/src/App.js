4327605eaf076d4338a46547732ba3f562827d1a
James Coughlin <mrjacoughlin@gmail.com>
2021-02-24 11:28:21 -0500

    Mounting and Unmounting

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,11 +1,25 @@
-import React, { Component } from "react";
-import "./styles.css";
+import logo from "./logo.svg";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Table from "react-bootstrap/Table";
 import { Col, Row } from "react-bootstrap";
+import { Component } from "react";
 
 export default class App extends Component {
-    state ={
-        people:[],
-        serach:""
-};
+  state = {
+    people: [],
+    search: "",
+  };
+  componentDidMount() {
+    console.log("mounted!");
+    const data = fetch("https://randomuser.me/api/?results=50");
+    data
+      .then((response) => response.json())
+      .then((response) => {
+        this.setState({ people: response.results });
+      });
+  }
+  componentWillUnmount() {
+    console.log("I will Unmount");
+  }
+}
