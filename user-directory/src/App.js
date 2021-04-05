a7e84a2807efa4cc6d1e729d4b190596a86c212b
James Coughlin <mrjacoughlin@gmail.com>
2021-04-05 12:31:33 -0400

    DataArea, state,headings handleSort,handleSearchChange

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Wrapper>
         <Header />
+        <Main />
       </Wrapper>
     </div>
   );
